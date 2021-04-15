6f412f522ac9d736ef5571282960d1729f12bf10
Sandra Lara <sandra.larafigueroa23@gmail.com>
2021-04-15 17:25:17 -0500

    modificacion index

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
 
     require_once "./config/APP.php";
     require_once "./controllers/ViewsController.php";
-  require_once "./controllers/house.controller.php";
-  require_once "./models/house.model.php";
 
+
+    
+   
+    require_once "./controllers/house.controller.php";
+    require_once "./models/house.model.php";
     $template = new ViewsController();
 
     $template->views_template_controller();
